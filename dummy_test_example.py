be508c6262b734d778576a79e76cc58d92ac29c7
Ahbubu <julia.vella.89@gmail.com>
2015-06-27 10:44:35 +0800

    Update dummy_test_example.py
    added capability to run NSlookup test

--- dummy_test_example.py
+++ dummy_test_example.py
@@ -1,5 +1,6 @@
 from tests.DummyTests import *
 from tests.WebSiteTest import WebSiteTest
+from test.DNStest import DNStest
 
 
 tests = [
@@ -8,7 +9,9 @@ tests = [
     DummyErrorTest("A dummy test that will raise an exception"),
     WebSiteTest("Check we can get to google", site="www.google.com"),
     WebSiteTest("Check we can get to cisco", site="www.cisco.com"),
-    WebSiteTest("Check we can get to microsoft", site="www.microsoft.com")]
+    WebSiteTest("Check we can get to microsoft", site="www.microsoft.com"),
+    DNStest('makes sure DNS works for youtubes load balancer', 'www.youtube.com', ['216.58.220.142']),
+    DNStest('makes sure DNS works for website', 'www.dropbox.com',  ['108.160.172.206', '108.160.172.238'])]
 
 passed = 0
 for test in tests:
